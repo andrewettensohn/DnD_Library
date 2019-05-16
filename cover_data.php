52968ce2d7377cf64af65eb258ef8ecee108207b
Andrew Ettensohn <47993107+andrewettensohn@users.noreply.github.com>
2019-05-16 18:36:16 -0400

    Add files via upload

---
+++ cover_data.php
@@ -0,0 +1,28 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<?php
+//get everything from the testJobs table and put it into an array for use in jobSearch.html
+
+ $connect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+ $database = mysqli_select_db($connect, DB_DATABASE);
+ $query = "SELECT * FROM cover";
+ $result = mysqli_query($connect, $query);
+ while($row = mysqli_fetch_assoc($result))
+ {
+    $data[] = $row;
+
+ }
+
+ function utf8ize($d) {
+   if (is_array($d)) {
+       foreach ($d as $k => $v) {
+           $d[$k] = utf8ize($v);
+       }
+   } else if (is_string ($d)) {
+       return utf8_encode($d);
+   }
+   return $d;
+}
+
+echo json_encode(utf8ize($data));
+
+?>
